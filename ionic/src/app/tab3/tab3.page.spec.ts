b29cde9d9b5bb5e9a7cc134e29128d3ce06ccbcd
luanagoncalvess <luanagoncalves@dcc.ufrj.br>
2019-08-16 10:45:01 -0300

    -Tab2=Cadastro

--- ionic/src/app/tab3/tab3.page.spec.ts
+++ ionic/src/app/tab3/tab3.page.spec.ts
@@ -3,7 +3,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { Tab3Page } from './tab3.page';
 
-describe('Tab3Page', () => {
+describe('Configurações', () => {
   let component: Tab3Page;
   let fixture: ComponentFixture<Tab3Page>;
 
